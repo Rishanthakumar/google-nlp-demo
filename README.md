ee20cb9ff2925040dd0e198d5875a8881583e710
Rishanthakumar <rishanthakumar@gmail.com>
2017-09-02 20:47:37 +0530

    Inital commit with overall sentiment and entity sentiment anlysis demonstration

---
+++ README.md
@@ -0,0 +1,24 @@
+# Demonstration of Google Cloud Natural Language API
+
+Google Cloud Natural Language API provides natural language
+understanding technologies to developers, including sentiment analysis, entity
+recognition, and syntax analysis. This API is part of the larger collection of
+Cloud Machine Learning APIs.
+
+These sample Java applications demonstrate how to access the Cloud Natural
+Language API.
+
+[language]: https://cloud.google.com/natural-language/docs/
+[google-cloud-java]: https://github.com/GoogleCloudPlatform/google-cloud-java
+
+## Quickstart
+
+(1) Install [Maven](http://maven.apache.org/).
+(2) Get the absolute path of the `NLPFirstProject.json` file, which is available in the project root folder.
+(3) Set the environment variable as follows
+
+```bash
+export GOOGLE_APPLICATION_CREDENTIALS=/pathTo/NLPFirstProject.json
+```
+
+(4) Run the main method in the `Sample.java` file.
---
+++ src/main/java/com/example/language/Sample.java
@@ -0,0 +1,71 @@
+package com.example.language;
+
+// [START language_quickstart]
+// Imports the Google Cloud client library
+
+import com.google.cloud.language.v1beta2.*;
+
+public class Sample {
+
+  public static void main(String... args) throws Exception {
+
+    // Instantiates a client
+    LanguageServiceClient languageApi = LanguageServiceClient.create();
+
+    // The text to analyze
+    String text = "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic " +
+            "Show.  Sundar Pichai said in his keynote that users love their new Android phones.";
+    Document doc = Document.newBuilder()
+            .setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+
+    // Sentiment
+    analyseSentiment(languageApi, doc);
+
+    //Entity Sentiment
+    analyseEntity(languageApi, doc);
+  }
+
+  /**
+   * Sample method to get the overall sentiment score
+   * @param languageApi
+   * @param doc
+   */
+  public static void analyseSentiment(LanguageServiceClient languageApi, Document doc) {
+
+    // Detects the sentiment of the text
+    Sentiment sentiment = languageApi.analyzeSentiment(doc).getDocumentSentiment();
+
+    System.out.println(" <------ GCP NLP Sentiment Analysis -----> ");
+
+    System.out.println();
+    System.out.println("Overall Score   : " + sentiment.getScore());
+    System.out.println();
+
+  }
+
+  /**
+   * Sample method to analyse the entities with sentiment in a text.
+   * @param languageApi
+   * @param doc
+   */
+  public static void analyseEntity(LanguageServiceClient languageApi, Document doc) {
+
+    AnalyzeEntitySentimentRequest request = AnalyzeEntitySentimentRequest.newBuilder()
+            .setDocument(doc)
+            .setEncodingType(EncodingType.UTF16).build();
+    AnalyzeEntitySentimentResponse response = languageApi.analyzeEntitySentiment(request);
+
+    System.out.println(" <------ GCP NLP Entity Sentiment Analysis -----> ");
+
+    for(Entity entity : response.getEntitiesList())
+    {
+      System.out.println();
+      System.out.println("Name      : " + entity.getName());
+      System.out.println("Type      : " + entity.getType().name());
+      System.out.println("Score     : " + entity.getSentimentOrBuilder().getScore());
+      System.out.println("Salience  : " + entity.getSentimentOrBuilder().getScore());
+      System.out.println();
+    }
+  }
+}
+// [END language_quickstart]
